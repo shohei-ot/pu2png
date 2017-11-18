4ddb74ad271f009b54c95d5095567b926c39e554
s_ot <shohei.ot@gmail.com>
2017-11-18 15:33:00 +0900

    changed app version, gitignore, test code

--- test/index.js
+++ test/index.js
@@ -1,11 +1,20 @@
 'use strict'
+const fs = require('fs')
+const path = require('path')
 // import pu2png from '../index.js'
 const pu2png = require('../index.js')
 
-pu2png.setBaseDir(__dirname).convert('test.pu', 'test.png').then((toPath) => {
+pu2png.convert('test.pu', 'test.png').then((toPath) => {
   console.log('### Success!: ' + toPath)
   return true
 }, (err) => {
   console.log('### Failed!: ' + err.message)
-  return false
+  try {
+    const toPath = path.resolve(__dirname, 'test.png')
+    fs.statSync(toPath)
+    fs.unlinkSync(toPath)
+  } catch (e) {
+    // 
+  }
+  throw err
 })
